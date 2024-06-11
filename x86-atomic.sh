1f073ed52162cfbf1016eccfcb9cb269d9967b14
KKiiim <jerryfoooster@gmail.com>
2024-06-11 21:03:59 +0800

    compare -O0 -O1

--- x86-atomic.sh
+++ x86-atomic.sh
@@ -1,1 +1,10 @@
-g++ -std=c++20 src/x86-atomic.cpp -o x86Atomic && ./x86Atomic && rm x86Atomic
+echo =======-O0=======
+g++ -std=c++20 -O0 src/x86-atomic.cpp -o x86Atomic && ./x86Atomic && rm x86Atomic
+echo =======-O1=======
+g++ -std=c++20 -O1 src/x86-atomic.cpp -o x86Atomic && ./x86Atomic && rm x86Atomic
+echo =======-O2=======
+g++ -std=c++20 -O2 src/x86-atomic.cpp -o x86Atomic && ./x86Atomic && rm x86Atomic
+echo =======-O3=======
+g++ -std=c++20 -O3 src/x86-atomic.cpp -o x86Atomic && ./x86Atomic && rm x86Atomic
+echo =======-Os=======
+g++ -std=c++20 -Os src/x86-atomic.cpp -o x86Atomic && ./x86Atomic && rm x86Atomic
